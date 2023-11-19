494deff7b3ec1ba3d4919ae43558e93a7b54a1b3
Jarrod Goodney <jarrod.goodney@gmail.com>
2023-11-19 15:47:58 -0500

    2nd commit. added contact form and other styles

---
+++ src/app/components/contact/contact.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import styles from './contact.module.css'
+
+export default function contact() {
+  return (
+    <div className={styles.contactContainer}>
+
+        <h2>Book an appointment!</h2>
+        <p>Any quetsions or concerns?</p>
+        <p>You can message us here as well or call us!</p>
+        <form>
+            <input type="text" value="name"/>
+            <input type="text" value="email"/>
+            <select id="cars" name="cars" placeholder="what kind of work do you need?">
+                <option value="volvo">Volvo</option>
+                <option value="saab">Saab</option>
+                <option value="fiat">Fiat</option>
+                <option value="audi">Audi</option>
+            </select>
+            <textarea></textarea>
+        </form>
+    </div>
+  )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Hero from './components/hero/hero'
 import About from './components/about/about'
+import Contact from './components/contact/contact'
 import Footer from './components/footer/footer'
 export default function Home() {
   return (
     <main >
       <Hero />
       <About />
+      <Contact />
       <Footer />
     </main>
   )
