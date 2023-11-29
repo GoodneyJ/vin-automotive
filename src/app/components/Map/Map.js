32629dabe90d9739c0fb481c85f4d427d22177cb
Jarrod Goodney <jarrod.goodney@gmail.com>
2023-11-29 18:48:13 -0500

    added maps fucntionality

---
+++ src/app/components/Map/Map.js
@@ -0,0 +1,50 @@
+'use client'
+import React, {useEffect, useRef} from 'react'
+import {Loader} from '@googlemaps/js-api-loader'
+export default function Map() {
+
+    const mapRef = React.useRef(null)
+
+    useEffect(() => {
+        const initMap = async () => {
+
+
+            const loader = new Loader({
+                apiKey: process.env.NEXT_PUBLIC_MAPS_API_KEY,
+                version: 'weekly',
+            })
+
+            const { Map } = await loader.importLibrary('maps');
+
+            const {Marker} = await loader.importLibrary('marker');
+
+            const position = {
+                lat: 39.113331,
+                lng: -76.83553
+            }
+
+            // map options
+            const mapOptions = google.maps.MapOptions = {
+                center: position,
+                zoom: 17,
+                mapId: '337c211febce61be'
+            }
+
+
+            //Setup the map
+            const map = new Map(mapRef.current, mapOptions);
+
+            // put up a marker
+            const marker = new Marker ({
+                map: map,
+                position: position
+            });
+        }
+
+        initMap()
+    }, [])
+  return (
+    <div style={{ height: '600px'}} ref={mapRef}/>
+
+  )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import About from './components/About/About'
 import ServiceSection from './components/ServiceSection/ServiceSection'
 import TutorialSection from './components/TutorialSection/TutorialSection'
 import Testimonials from './components/Testimonials/Testimonials'
+import Map from './components/Map/Map'
 import Contact from './components/Contact/Contact'
 import Footer from './components/Footer/Footer'
 export default function Page() {
@@ -13,6 +14,7 @@ export default function Page() {
       <ServiceSection />
       <TutorialSection />
       <Testimonials />
+      <Map />
       <Contact />
       <Footer />
     </main>
